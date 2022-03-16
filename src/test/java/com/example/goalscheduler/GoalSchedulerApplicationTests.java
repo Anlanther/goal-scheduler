80ce4bfe28e56316592bcdf5283716c774f8cd08
Anlanther <25150182+Anlanther@users.noreply.github.com>
2022-03-16 22:07:22 +0800

    created test file

---
+++ src/test/java/com/example/goalscheduler/GoalSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goalscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoalSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
